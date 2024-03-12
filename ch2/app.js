8dafee37e39047535287b78bb5cd415bec69e0f2
Ruslan <veb3solidity@gmail.com>
2024-03-12 19:13:26 +0200

    less41_30

--- ch2/app.js
+++ ch2/app.js
@@ -8,5 +8,11 @@ Vue.createApp({
             age: 43
         },
         num: [1,2,3,4,5,6]
-    })
+    }),
+    computed: {
+       eventItems(){
+        return this.num.filter(i => i%2)
+       }
+    }
+
 }).mount('#app');
